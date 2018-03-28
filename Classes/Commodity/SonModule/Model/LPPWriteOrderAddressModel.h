92d3e729d23058bc0906e6dd6a54129754ac88b7
zcx2016 <chinazhangchenxi@163.com>
2018-03-28 10:12:12 +0800

    first commit

---
+++ Classes/Commodity/SonModule/Model/LPPWriteOrderAddressModel.h
@@ -0,0 +1,13 @@
+//
+//  LPPWriteOrderAddressModel.h
+//  LPPushClient
+//
+//  Created by 张晨曦 on 2018/3/27.
+//  Copyright © 2018年 张晨曦. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LPPWriteOrderAddressModel : NSObject
+
+@end
---
+++ Classes/Commodity/SonModule/Model/LPPWriteOrderAddressModel.m
@@ -0,0 +1,13 @@
+//
+//  LPPWriteOrderAddressModel.m
+//  LPPushClient
+//
+//  Created by 张晨曦 on 2018/3/27.
+//  Copyright © 2018年 张晨曦. All rights reserved.
+//
+
+#import "LPPWriteOrderAddressModel.h"
+
+@implementation LPPWriteOrderAddressModel
+
+@end
